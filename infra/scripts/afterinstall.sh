bbed26b1f34b1a82bc35fb8e197d5aac019f94e3
Dongeun Paeng <dongeun.paeng@gmail.com>
2024-11-02 17:39:49 +0900

    033: ci/cd (working CodeDeploy)

--- infra/scripts/afterinstall.sh
+++ infra/scripts/afterinstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod +x /home/ec2-user/afterinstall.sh
-echo '{"type": "module"}' > /home/ec2-user/build/package.json
+echo '{"type": "module"}' > /home/ec2-user/sveltekit-test/build/package.json
