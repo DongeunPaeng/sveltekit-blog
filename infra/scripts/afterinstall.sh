c76542b5147f4ded0b76f2602cc2584e2f521c4b
Dongeun Paeng <dongeun.paeng@gmail.com>
2024-11-03 23:36:10 +0900

    041: ci/cd (build test)

--- infra/scripts/afterinstall.sh
+++ infra/scripts/afterinstall.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 chmod +x /home/ec2-user/afterinstall.sh
-echo '{"type": "module"}' > /home/ec2-user/sveltekit-test/build/package.json
+cd /home/ec2-user/afterinstall.sh || return
+pnpm install
