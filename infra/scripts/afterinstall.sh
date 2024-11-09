e20daa90b21620c786d649261ca81b9e8c357e91
Dongeun Paeng <dongeun.paeng@gmail.com>
2024-11-10 00:20:55 +0900

    054: apply pm2

--- infra/scripts/afterinstall.sh
+++ infra/scripts/afterinstall.sh
@@ -2,7 +2,7 @@
 export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 chmod +x /home/ec2-user/sveltekit-test/build/afterinstall.sh
-sudo chown -R ec2-user:ec2-user /home/ec2-user/sveltekit-test/**/*
+sudo chown -R ec2-user:ec2-user /home/ec2-user/sveltekit-test/build
 cd /home/ec2-user/sveltekit-test/build || exit
 pnpm install
 pm2 restart sveltekit-app || pm2 start index.js --name sveltekit-app --watch
