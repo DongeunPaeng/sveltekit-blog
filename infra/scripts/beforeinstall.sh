a98ef4464d5ffc0521bf53eb0128e0980622a187
Dongeun Paeng <dongeun.paeng@gmail.com>
2024-11-10 00:08:28 +0900

    054: apply pm2

--- infra/scripts/beforeinstall.sh
+++ infra/scripts/beforeinstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -rf /home/ec2-user/sveltekit-test/build
-chown -R ec2-user:ec2-user /home/ec2-user/sveltekit-test
+sudo rm -rf /home/ec2-user/sveltekit-test/build
+sudo chown -R ec2-user:ec2-user /home/ec2-user/sveltekit-test/**/*
